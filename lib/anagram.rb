ff6739c1ac5346b0bbcfafc7931213bfa4c06d6e
Emily Harber <emily.a.harber@gmail.com>
2019-10-03 22:50:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,14 @@
-# Your code goes here!
 class Anagram 
   attr_accessor :word 
   
   def initialize(word)
     @word = word 
+    match
   end 
   
   def match(some_words)
-    
+    some_words.each do |new_word|
+      new_word.split("") == @word.split("")
+    end 
   end 
 end 
